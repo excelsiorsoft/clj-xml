36e0a083b09dd6b20deee18ba4a88cab571fa140
sleyzerzon <senya22@yahoo.com>
2016-08-31 09:20:12 -0400

    repackaging

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.excelsiorsoft.clj-xml "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://com.excelsiorsoft/clj-xml"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
