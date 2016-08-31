7c63de9a2797f13266c316df4e46198de4847060
sleyzerzon <senya22@yahoo.com>
2016-08-31 18:03:33 -0400

    setting up tests

--- test/com/excelsiorsoft/clj_xml/validation_test.clj
+++ test/com/excelsiorsoft/clj_xml/validation_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [com.excelsiorsoft.clj-xml.validation :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def validate (validate-against "resources/test.xsd"))
+
+(deftest inline-xml
+  (testing "valid xml is  passed in as as string")
+  (is (= true (validate "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addresses xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n xsi:noNamespaceSchemaLocation='test.xsd'>\n\n <address>\n <name>Joe Tester</name>\n <street>Baker street 5</street>\n </address>\n\n</addresses>"))))
