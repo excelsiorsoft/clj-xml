53964b4e1ea4bf0e65ab232f7d7409508aa7de20
sleyzerzon <senya22@yahoo.com>
2016-08-31 19:24:37 -0400

    more tests

--- test/com/excelsiorsoft/clj_xml/validation_test.clj
+++ test/com/excelsiorsoft/clj_xml/validation_test.clj
@@ -7,3 +7,18 @@
 (deftest inline-xml
   (testing "valid xml is  passed in as as string")
   (is (= true (validate "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addresses xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n xsi:noNamespaceSchemaLocation='test.xsd'>\n\n <address>\n <name>Joe Tester</name>\n <street>Baker street 5</street>\n </address>\n\n</addresses>"))))
+
+(deftest inline-wrong-xml
+  (testing "valid xml is  passed in as as string")
+  (is (= false (validate "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<addresses xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\n xsi:noNamespaceSchemaLocation='test.xsd'>\n\n <address>\n <abc/><name>Joe Tester</name>\n <street>Baker street 5</street>\n </address>\n\n</addresses>"))))
+
+
+(deftest ouside-xml
+  (testing "valid xml is  passed in as as an external file")
+  (is (= true (validate (slurp "resources/valid.xml")))))
+
+(deftest ouside-xml
+  (testing "valid xml is  passed in as as an external file")
+  (is (= true (validate (slurp "resources/valid.xml"))))
+  (is (= true (validate (slurp "resources/valid.xml")))))
+
